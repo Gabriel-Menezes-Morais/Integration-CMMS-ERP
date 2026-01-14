7ab27c30c3c710896837a7454fba4f9ec68ac013
Gabriel Menezes <gabriel.menezes.mata@gmail.com>
2026-01-14 09:32:31 -0300

    Change pyodbc to sqlalchemy.

--- funcoesBD.py
+++ funcoesBD.py
@@ -1,9 +1,8 @@
-import pyodbc #MUDAR DEPOIS PARA SQLALCHEMY
 import pandas as pd
 from dotenv import load_dotenv
 import os
 import logging
-
+import sqlalchemy
 logger_Alerta = logging.getLogger("app")
 
 # Carrega o arquivo .env
@@ -27,81 +26,67 @@ def conexao():
     
     #Cria e retorna uma conexão nova com o SQL Server
     try:
-        conn = pyodbc.connect(dados_conexao)
+        engine = sqlalchemy.create_engine(f"mssql+pyodbc:///?odbc_connect={dados_conexao}", fast_executemany=True)
     except Exception as e:
         logger_Alerta.exception(f"Erro ao conectar ao banco de dados: {e}")
         raise e
-    return conn
+    return engine
 
 def carrinho():
     #Verifica e armazena em df os itens que estão na tabela de Carrinho, i.e., os quais passaram pela geração de necessidades
     try:
-        conn = conexao()  # Tenta estabelecer a conexão
+        engine = conexao()  # Tenta estabelecer a conexão
         query = "SELECT * FROM dbo.CadNecComCOPY WHERE IDPedCom IS NULL"
-        df = pd.read_sql(query, conn)
+        df = pd.read_sql(query, engine)
     except Exception as e:
         logger_Alerta.exception(f"Erro ao obter dados do carrinho: {e}")
         return None  # Retorna None ou uma lista vazia, dependendo do que faz sentido no seu contexto
-    finally:
-        if 'conn' in locals():  # Verifica se a conexão foi criada
-            conn.close()  # Garante que a conexão seja fechada
     return df
 
 def carrinho_full_filtrado():
     #Aqui, selecionamos os itens de necessidade que são somente da categoria de manutenção
     try:
-        conn = conexao()  # Tenta estabelecer a conexão
+        engine = conexao()  # Tenta estabelecer a conexão
         query = """SELECT *
                     From dbo.CadNecComCOPY AS E
                     INNER JOIN dbo.CadProCOPY AS P
                     ON E.CodProCOPY = P.REF
                     WHERE P.GrupoH = 3969"""
-        df = pd.read_sql(query, conn)
+        df = pd.read_sql(query, engine)
     except Exception as e:
         logger_Alerta.exception(f"Erro ao obter dados filtrados do carrinho: {e}")
         return None  # Retorna None ou uma lista vazia
-    finally:
-        if 'conn' in locals():
-            conn.close()  # Garante que a conexão seja fechada
     return df
 
 def deletar_item_carrinho(CODIGOS):
     # Deleta itens que foram necessitados e já estão na tabela dbo.CadNecComCOPY
     try:
-        conn = conexao()  # Tenta estabelecer a conexão
-        cursor = conn.cursor()
+        engine = conexao()  # Tenta estabelecer a conexão
 
-        # Use = ? e executemany com um parâmetro por execução para evitar problemas com IN (?)
-        query = "DELETE FROM dbo.CadNecComCOPY WHERE CodProCOPY = ?"
+        query = "DELETE FROM dbo.CadNecComCOPY WHERE CodProCOPY = :cod"
 
-        for i in range(len(CODIGOS)):
-            CODIGOS[i] = [CODIGOS[i]]
+        parametros = [{"cod_id": item} for item in CODIGOS]
 
-        cursor.executemany(query, CODIGOS)
+        with engine.begin() as conn:
+            conn.execute(query, parametros)
 
-        conn.commit()
     except Exception as e:
         logger_Alerta.exception(f"Erro ao deletar itens do carrinho: {e}")
-    finally:
-        cursor.close()
-        if 'conn' in locals():
-            conn.close()  # Garante que a conexão seja fechada
     return
 
 def compra_item(datas):
     # Adiciona itens selecionados na tabela carrinho
     try:
-        conn = conexao()  # Tenta estabelecer a conexão
-        cursor = conn.cursor()
+        engine = conexao()  # Tenta estabelecer a conexão
 
-        sql_insert = "INSERT INTO dbo.CadNecComCOPY (CodProCOPY, Qtd) VALUES (?, ?)"
-        cursor.executemany(sql_insert, datas)
+        sql_insert = "INSERT INTO dbo.CadNecComCOPY (CodProCOPY, Qtd) VALUES (:cod, :qtd)"
 
-        conn.commit()
+        parametros = [{"cod": item[0], "qtd": item[1]} for item in datas]
+
+        with engine.begin() as conn:
+            conn.execute(sql_insert, parametros)
+
     except Exception as e:
         logger_Alerta.exception(f"Erro ao adicionar itens ao carrinho: {e}")
-    finally:
-        cursor.close()
-        if 'conn' in locals():
-            conn.close()  # Garante que a conexão seja fechada
+        
     return
